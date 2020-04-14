78c37d91565bb58f505ec95cb16a3e6ba102cb15
heeyeah <gdshy221@gmail.com>
2020-04-14 17:57:54 +0900

    init shbcloud employee project

---
+++ shbcloud-employee/src/main/kotlin/shb/cloud/employee/ShbcloudEmployeeApplication.kt
@@ -0,0 +1,11 @@
+package shb.cloud.employee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShbcloudEmployeeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShbcloudEmployeeApplication>(*args)
+}
---
+++ shbcloud-employee/src/test/kotlin/shb/cloud/employee/ShbcloudEmployeeApplicationTests.kt
@@ -0,0 +1,13 @@
+package shb.cloud.employee
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShbcloudEmployeeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
